206cf8667e1e94759aa03858a6c9f6dcc79bc232
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2017-08-11 09:19:02 -0700

    revert "ipv4: Should use consistent conditional judgement for ip fragment in __ip_append_data and ip_finish_output"
    This reverts commit f102bb7164c9020e12662998f0fd99c3be72d4f6 which is
    commit 0a28cfd51e17f4f0a056bcf66bfbe492c3b99f38 upstream as there is
    another patch that needs to be applied instead of this one.
    
    Cc: Zheng Li <james.z.li@ericsson.com>
    Cc: David S. Miller <davem@davemloft.net>
    Cc: Sasha Levin <alexander.levin@verizon.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/ipv4/ip_output.c
+++ net/ipv4/ip_output.c
@@ -888,17 +888,16 @@ static int __ip_append_data(struct sock *sk,
 		csummode = CHECKSUM_PARTIAL;
 
 	cork->length += length;
-        if (((length + fragheaderlen) > mtu || (skb && skb_is_gso(skb))) &&
-            (sk->sk_protocol == IPPROTO_UDP) &&
-            (rt->dst.dev->features & NETIF_F_UFO) && !rt->dst.header_len &&
-            (sk->sk_type == SOCK_DGRAM)) {
-                err = ip_ufo_append_data(sk, queue, getfrag, from, length,
-                                         hh_len, fragheaderlen, transhdrlen,
-                                         maxfraglen, flags);
-                if (err)
-                        goto error;
-                return 0;
-        
+	if (((length > mtu) || (skb && skb_is_gso(skb))) &&
+	    (sk->sk_protocol == IPPROTO_UDP) &&
+	    (rt->dst.dev->features & NETIF_F_UFO) && !rt->dst.header_len &&
+	    (sk->sk_type == SOCK_DGRAM)) {
+		err = ip_ufo_append_data(sk, queue, getfrag, from, length,
+					 hh_len, fragheaderlen, transhdrlen,
+					 maxfraglen, flags);
+		if (err)
+			goto error;
+		return 0;
 	}
 	/* So, what's going on in the loop below?
 	 *
