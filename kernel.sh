20f366c19bc1bbe6e8422c86877041558282f13f
Shimon <sesasoh@gmail.com>
2024-09-18 23:01:29 +0200

    pme: adjust some things in kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,9 +1,13 @@
 export ARCH=arm64
 export CROSS_COMPILE="/home/j6idot/HTC/gcc-linaro-4.9.4/bin/aarch64-linux-gnu-"
-export LOCALVERSION="$(date +%Y-%m-%d--%H-%M-%S)-j6idot"
+export LOCALVERSION="-$(date +%Y-%m-%d-%H-%M-%S)"
 
-make clean || { echo 'build failed!' ; exit 1; }
+make clean
 make pme_defconfig O=out -j$(nproc --all) || { echo 'build failed!' ; exit 1; }
-make || { echo 'build failed!' ; exit 1; }
+make O=out || { echo 'build failed!' ; exit 1; }
 
+echo -
+echo -
+echo #  Copying kernel
 cp out/arch/arm64/boot/Image.gz-dtb ~/HTC/KernelBuilds/LiteEKernel-$(date +%Y-%m-%d--%H-%M-%S)
+echo #  Done !
