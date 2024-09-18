ab295f2fe556b2f3f87f63f03e0a0a477a387291
Shimon <sesasoh@gmail.com>
2024-09-18 23:28:00 +0200

    pme: adjust kernel.sh and create an anykernel zip

--- kernel.sh
+++ kernel.sh
@@ -1,6 +1,10 @@
+# Personal Build Script
+
 export ARCH=arm64
 export CROSS_COMPILE="/home/j6idot/HTC/gcc-linaro-4.9.4/bin/aarch64-linux-gnu-"
 export LOCALVERSION="-$(date +%Y-%m-%d-%H-%M-%S)"
+export PMEVERSION="TESTING"
+export ROOTDIR="/home/j6idot/HTC/HTCStock"
 
 make clean
 make pme_defconfig O=out -j$(nproc --all) || { echo 'build failed!' ; exit 1; }
@@ -8,6 +12,7 @@ make O=out || { echo 'build failed!' ; exit 1; }
 
 echo -
 echo -
-echo #  Copying kernel
-cp out/arch/arm64/boot/Image.gz-dtb ~/HTC/KernelBuilds/LiteEKernel-$(date +%Y-%m-%d--%H-%M-%S)
-echo #  Done !
+echo #  Making an anykernel compatible zip
+cp $ROOTDIR/out/arch/arm64/boot/Image.gz-dtb ~/HTC/KernelBuilds/AnyKernel/zImage
+bash ~/HTC/KernelBuilds/anykernel.sh
+cd ~/HTC/HTCStock
