69f39f22ba04be2186f5c72a9b9e1b04430e9174
Shimon <sesasoh@gmail.com>
2024-10-27 16:18:57 +0100

    pme: update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,17 +1,16 @@
 # Personal Build Script
 
-# Build Counter
-touch ~/HTC/KernelBuilds/count
-COUNTER=$(cat ~/HTC/KernelBuilds/count)
-next_n=$[$COUNTER+1]
-echo $next_n > ~/HTC/KernelBuilds/count
-#
-
 export ARCH=arm64
 export CROSS_COMPILE="/home/j6idot/HTC/gcc-linaro-6.5.0/bin/aarch64-linux-gnu-"
-export LOCALVERSION="-$(date +%Y-%m-%d-%H-%M-%S)-build-$COUNTER-LINARO6.5"
 export PMEVERSION="TESTING"
 export ROOTDIR="/home/j6idot/HTC/HTCStock"
+#
+touch ~/HTC/KernelBuilds/count
+COUNTER=$(cat ~/HTC/KernelBuilds/count)
+# Variables
+export LOCALVERSION="-J6idotKernel-$(date +%Y-%m-%d-%H-%M-%S)-build-$COUNTER-UPSTREAM"
+
+
 
 # Make clean
 read -p "Do you know to clean the output folder? (y/n) " RESP
@@ -24,6 +23,11 @@ fi
 
 make pme_defconfig O=out -j$(nproc --all) || { echo 'build failed!' ; exit 1; }
 make O=out || { echo 'build failed!' ; exit 1; }
+
+# Build Counter
+next_n=$[$COUNTER+1]
+echo $next_n > ~/HTC/KernelBuilds/count
+#
 
 echo -
 echo -
